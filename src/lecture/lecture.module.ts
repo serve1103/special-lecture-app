c9c019806c36ead58a8229ac0e1dd7da5a199ffc
Tom(한재섭) <serve1103@jinhakapply.com>
2024-06-24 10:56:05 +0900

    DB connection

--- src/lecture/lecture.module.ts
+++ src/lecture/lecture.module.ts
@@ -1,15 +1,9 @@
 import { Module } from '@nestjs/common';
 import { LectureController } from './presentation/controllers/lecture.controller';
 import { LectureServiceImpl } from './application/services/lecture.serivce';
-import { LectureService } from './application/services/lecture.service.interface';
 
 @Module({
   controllers: [LectureController],
-  providers: [
-    { provide: LectureService, useClass: LectureServiceImpl },
-  ],
-  exports: [
-    LectureService,
-  ]
+  providers: [{ provide: 'LectureService', useClass: LectureServiceImpl }],
 })
 export class LectureModule {}
